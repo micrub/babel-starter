b307b3b01a33c7bdfebd2b2caa53943d95a37efc
Michael Rubanov <mrubanov@gmail.com>
2017-07-18 00:57:30 +0300

    Testing setup using `mocha` and `babel-register`, which allows usage of babel as on fly compiler for `mocha`.

---
+++ test/IndexSpec.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import Core from '../src/';
+
+describe('Core', () => {
+
+  it('Module should be an instance of `Object`', () => {
+    expect(Core).to.be.instanceOf(Object)
+  });
+
+});
